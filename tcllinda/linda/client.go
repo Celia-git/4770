575b1d8392ba6dd0cb45f2f23babe233709821ca
Celia-git <celia.hough.programmer@gmail.com>
2025-09-16 19:02:51 -0500

    updated client

--- tcllinda/linda/client.go
+++ tcllinda/linda/client.go
@@ -4,13 +4,13 @@ import (
     "encoding/json"
     "flag"
     "fmt"
-    "reflect"
     "net"
     "os"
     "strconv"
     "strings"
 )
 
+// Parse tuple arguments into a slice of interface{}
 func parseTuple(args []string) []interface{} {
     tuple := make([]interface{}, 0, len(args))
     for _, arg := range args {
@@ -48,6 +48,40 @@ func parseTuple(args []string) []interface{} {
     return tuple
 }
 
+// Print operation and tuple values in natural language
+func describeTuple(op string, tuple []interface{}) {
+    desc := ""
+    for i, v := range tuple {
+        if i > 0 {
+            desc += " and "
+        }
+        switch val := v.(type) {
+        case string:
+            if val == "?" {
+                desc += "wildcard"
+            } else {
+                desc += fmt.Sprintf("string \"%v\"", val)
+            }
+        case int64:
+            desc += fmt.Sprintf("int64 %d", val)
+        case float64:
+            desc += fmt.Sprintf("float64 %v", val)
+        case nil:
+            desc += "wildcard"
+        default:
+            desc += fmt.Sprintf("%T %v", v, v)
+        }
+    }
+    switch op {
+    case "out":
+        fmt.Printf("Tuple with %s stored in tuple space\n", desc)
+    case "in":
+        fmt.Printf("Tuple with %s deleted (consumed) from tuple space\n", desc)
+    case "rd":
+        fmt.Printf("Tuple with %s read (but not removed) from tuple space\n", desc)
+    }
+}
+
 func main() {
     host := flag.String("host", "localhost:8080", "server address")
 
@@ -68,41 +102,43 @@ func main() {
     decoder := json.NewDecoder(conn)
 
     var req map[string]interface{}
+    var tuple []interface{}
+    var op string
+
     if *outFlag != "" {
-        tuple := parseTuple(strings.Fields(*outFlag))
+        tuple = parseTuple(strings.Fields(*outFlag))
         req = map[string]interface{}{"cmd": "out", "tuple": tuple}
-        fmt.Printf("Insert Tuple: ")
+        op = "out"
     } else if *inFlag != "" {
-        tuple := parseTuple(strings.Fields(*inFlag))
-        // convert "?" to nil for wildcard
+        tuple = parseTuple(strings.Fields(*inFlag))
         for i, v := range tuple {
             if s, ok := v.(string); ok && s == "?" {
                 tuple[i] = nil
             }
         }
         req = map[string]interface{}{"cmd": "in", "pattern": tuple}
-        fmt.Printf("Delete Tuple: ")
+        op = "in"
     } else if *rdFlag != "" {
-        tuple := parseTuple(strings.Fields(*rdFlag))
+        tuple = parseTuple(strings.Fields(*rdFlag))
         for i, v := range tuple {
             if s, ok := v.(string); ok && s == "?" {
                 tuple[i] = nil
             }
         }
         req = map[string]interface{}{"cmd": "rd", "pattern": tuple}
-        fmt.Printf("Read Tuple: ")
+        op = "rd"
     } else {
         fmt.Println("Error: must specify one of -out, -in, or -rd")
         os.Exit(1)
     }
 
-    // Endcode request to send
+    describeTuple(op, tuple)
+
     if err := encoder.Encode(req); err != nil {
         fmt.Println("Failed to send request:", err)
         os.Exit(1)
     }
 
-    // Decode request to receive
     var resp map[string]interface{}
     if err := decoder.Decode(&resp); err != nil {
         fmt.Println("Failed to read response:", err)
@@ -114,9 +150,6 @@ func main() {
     } else if result, ok := resp["result"]; ok && result != nil {
         fmt.Println("Result:", result)
     } else {
-        for i, v := range tuple{
-            fmt.Printf("%s %v, ", reflect.TypeOf(v), v)
-        }
+        fmt.Println("OK")
     }
 }
-
